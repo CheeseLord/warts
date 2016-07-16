a385b114099998bd36979ea3eca3f7407b914596
Joel O <joelornstein@gmail.com>
2016-07-16 16:56:43 -0600

    updated argparse commands

--- src/twisted_test.py
+++ src/twisted_test.py
@@ -4,10 +4,14 @@ import sys
 from test_echoserver import runEchoServer
 from test_echoclient import runEchoClient
 
+def isServer(args):
+    return args.command == "server"
+
+
 def runTwistedTest():
     args = parseArguments()
 
-    if args.isServer:
+    if isServer(args):
         print "Running echo server..."
         runEchoServer()
     else:
@@ -18,10 +22,9 @@ def runTwistedTest():
 def parseArguments():
     parser = argparse.ArgumentParser()
 
+    # Positional arguements
     # Are we the server or the client?
-    parser.add_argument('--server', dest='isServer', action='store_true',
-                        default=False)
-    parser.add_argument('--client', dest='isServer', action='store_false')
+    parser.add_argument('command', choices=["server", "client"])
 
     # Server parameters, if we're the client.
     parser.add_argument('--host', type=str, nargs='?', default="127.0.0.1")
