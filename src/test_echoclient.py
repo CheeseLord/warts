712918aaf278b4764d3c166705a090b192506839
Greg Kronmiller <kronmillerg@gmail.com>
2016-07-23 14:13:07 -0700

    Connect the stdio handler to the echo client.
    Messages you type on stdin are now sent to the server. Unless you type
    them too fast, in which case the whole client comes crashing down.

--- src/test_echoclient.py
+++ src/test_echoclient.py
@@ -12,18 +12,39 @@ from twisted.internet.protocol import ClientFactory
 from twisted.protocols.basic import Int16StringReceiver, LineReceiver
 
 
+theClient = None
+
 
 class EchoClient(Int16StringReceiver):
     end = "Bye-bye!"
 
+    def __init__(self):
+        # Apparently Int16StringReceiver doesn't have an __init__.
+        # Int16StringReceiver.__init__(self)
+
+        # FIXME FIXME FIXME: This is a terrible hack, which doesn't even quite
+        # work. If you type something before we've finished connecting, then
+        # the stdio handler will try to send it using theClient even though
+        # theClient isn't actually initialized yet, so you'll get:
+        #     AttributeError: 'NoneType' object has no attribute 'sendString'
+        # Really I think we should be setting up a deferred that waits to
+        # create the stdio handler until the client object is already created,
+        # so that the stdio handler can safely reference the client object from
+        # the start. But I'm not really sure how to do that, so for now we get
+        # this hack instead.
+        global theClient
+        theClient = self
+
     def connectionMade(self):
-        self.sendString("Hello, world!\n")
-        self.sendString("What a fine day it is.\n")
-        self.sendString(self.end + "\n")
+        self.sendString("Hello, world!")
+        self.sendString("What a fine day it is.")
+        self.sendString(self.end)
 
 
     def stringReceived(self, line):
-        print("receive:", line[:-1])
+        # TODO: Probably this should go through the StdioHandler rather than
+        # calling print directly....
+        print("[receive]", line)
         # if line == self.end:
         #     self.transport.loseConnection()
 
@@ -50,10 +71,11 @@ class StdioHandler(LineReceiver):
     delimiter = os.linesep
 
     def connectionMade(self):
-        self.sendLine("Connected, yay!")
+        self.sendLine("Stdio handler created, yay!")
 
     def lineReceived(self, line):
-        self.sendLine("You typed: '{}'".format(line))
+        theClient.sendString(line)
+        self.sendLine("[send]    {}".format(line))
 
 
 def runEchoClientHelper(reactor, host, port):
