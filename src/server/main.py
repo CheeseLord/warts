e8550629df1becb7a472ef1d7244d8ffb0639ef0
Joel O <joelornstein@gmail.com>
2016-08-06 16:34:49 -0600

    added chat server

--- src/server/main.py
+++ src/server/main.py
@@ -1,5 +1,7 @@
 from src.shared.dummy import sharedFunctionality
+from src.server.networking import runServer
 
 def main(args):
-    print "Hi, I'm a server!"
+    print "Hi, I'm a sever!"
     sharedFunctionality()
+    runServer(args.port)
