7a665661575627b622fe936eb4cd2daa786cba30
Kyle Fox <fox_kyle@yahoo.com>
2016-09-10 18:24:27 -0400

    [#2] Add color to logs.

--- src/shared/logconfig.py
+++ src/shared/logconfig.py
@@ -1,3 +1,22 @@
-import logging
+# import logging
+#
+# handler = logging.StreamHandler()
 
-handler = logging.StreamHandler()
+import colorlog
+
+handler = colorlog.StreamHandler()
+formatter = colorlog.ColoredFormatter(
+    "%(log_color)s%(levelname)-8s%(reset)s %(blue)s%(message)s",
+    datefmt=None,
+    reset=True,
+    log_colors={
+        'DEBUG':    'cyan',
+        'INFO':     'green',
+        'WARNING':  'yellow',
+        'ERROR':    'red',
+        'CRITICAL': 'red,bg_white',
+    },
+    secondary_log_colors={},
+    style='%'
+)
+handler.setFormatter(formatter)
