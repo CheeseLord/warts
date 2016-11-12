a18a5fb27d9f58a5cacfce11dc46936824a70ac3
Greg Kronmiller <kronmillerg@gmail.com>
2016-11-12 15:22:25 -0800

    Add functions to convert world <--> chunk coords.

--- src/shared/config.py
+++ src/shared/config.py
@@ -1,2 +1,5 @@
 # Intended length of one game tick, in seconds. (Actual length may vary.)
 TICK_LENGTH = 0.1
+
+# Side length of a chunk, in world coordinates.
+CHUNK_SIZE = 60
---
+++ src/shared/geometry.py
@@ -0,0 +1,18 @@
+"""
+Functions for doing geometry calculations in the various types of coordinates
+shared between client and server.
+"""
+
+from shared.config import CHUNK_SIZE
+
+def chunkToWorld(chunkPos):
+    """
+    Return the world coordinates of the origin corner of chunkPos.
+    """
+    return map(lambda x: x * CHUNK_SIZE, chunkPos)
+
+def worldToChunk(worldPos):
+    """
+    Return the chunk coordinates of the chunk containing worldPos.
+    """
+    return map(lambda x: x / CHUNK_SIZE, chunkPos)
