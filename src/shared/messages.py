afb4c6b1c374c643bfc07c6258bc61113275bce5
Greg Kronmiller <kronmillerg@gmail.com>
2016-11-12 14:57:54 -0800

    Rework the definitions of the messages.
    Separate posArgs into 4 types, for the 4 different types of coordinates:
      - wPosArg for world coordinates
      - bPosArg for build coordinates
      - cPosArg for chunk coordinates
      - gPosArg for graphics coordinates (client only)
    
    Refactor the way the ArgumentSpecifications are described, so that
    different argument types that parse the same can be combined (ex:
    {w,b,c}PosArgs are all just pairs of ints).
    
    Fix Click being defined in terms of world coordinates (integers) instead
    of graphics coordinates (floats), and reintroduce the code we used to
    have to parse a pair of floats.
    
    Add a new message GroundInfo, which will eventually be used for the
    server to send information to the client about what type of ground is
    present in a particular chunk. Not used yet.

--- src/shared/messages.py
+++ src/shared/messages.py
@@ -7,35 +7,88 @@ from src.shared.message_infrastructure import defineMessageType, \
 ###############################################################################
 # Helper functions used by some of the argument specifications
 
-def encodePos(pos):
-    assert len(pos) == 2
+# Int pair
+
+def encodeIntPair(pair):
+    assert len(pair) == 2
     # For now, force to int before str()ing the coordinates so that if old code
     # is still using floats, it doesn't mess up the parser on the other end. We
     # may want to eventually remove that cast; I'm not sure.
-    return [str(int(x)) for x in pos]
+    return [str(int(x)) for x in pair]
 
-def parsePos(descs):
+def parseIntPair(descs):
     assert len(descs) == 2
     return tuple(map(int, descs))
+
+
+# Float pair
+
+def encodeFloatPair(pair):
+    assert len(pair) == 2
+    # Not sure if the cast to float here is really needed...
+    return [str(float(x)) for x in pair]
+
+def parseFloatPair(descs):
+    assert len(descs) == 2
+    return tuple(map(parseFloat, descs))
+
+def parseFloat(desc):
+    try:
+        val = float(desc)
+        if not isfinite(val):
+            raise ValueError
+        return val
+    except ValueError:
+        # Convert any exceptions raised during parsing into
+        # InvalidMessageErrors, so that we'll handle them correctly and not
+        # crash due to an invalid external message.
+        #
+        # Note: this isn't really quite right -- the full message isn't desc,
+        # but rather something like "set_pos 2 {desc} 1.6" -- but we don't have
+        # access to the full message, so this is the best we can do.
+        raise InvalidMessageError(desc, "Could not parse floating-point value")
+
+def isfinite(x):
+    return not math.isinf(x) and not math.isnan(x)
 
 
 ###############################################################################
 # Argument specifications
 
-idArg  = ArgumentSpecification(1, int)
-# TODO: Distinguish screenPos from worldPos
-posArg = ArgumentSpecification(2, parsePos, encodePos)
+# Specifications based on the underlying types. Don't use these directly; use
+# the better-named ones that are aliases of these. These exist so that (for
+# example) the three different type of pos args that all boil down to "pair of
+# ints" can be implemented as a single underlying specification.
+intArg       = ArgumentSpecification(1, int)
+intPairArg   = ArgumentSpecification(2, parseIntPair, encodeIntPair)
+floatPairArg = ArgumentSpecification(2, parseFloatPair, encodeFloatPair)
+
+# For player IDs.
+idArg = intArg
+
+# Different types of coordinates. For now at least, all bug graphics are
+# encoded and parsed exactly the same way.
+wPosArg = intPairArg    # World position (unit movements)
+bPosArg = intPairArg    # Build position (build grid)
+cPosArg = intPairArg    # Chunk position (terrain tiles)
+gPosArg = floatPairArg  # Graphics position (intra-client messages only)
+
+# The type of ground on a certain chunk.
+terrainTypeArg = intArg
 
 
 ###############################################################################
 # The messages themselves
 
-Click         = defineMessageType("click", [("pos", posArg)])
+Click         = defineMessageType("click", [("pos", gPosArg)])
 DeleteObelisk = defineMessageType("delete_obelisk", [("playerId", idArg)])
-MoveTo        = defineMessageType("move_to", [("dest", posArg)])
+GroundInfo    = defineMessageType("ground_info",
+                                  [("pos", cPosArg),
+                                   ("terrainType", terrainTypeArg)])
+MoveTo        = defineMessageType("move_to", [("dest", wPosArg)])
 NewObelisk    = defineMessageType("new_obelisk",
-                                  [("playerId", idArg), ("pos", posArg)])
+                                  [("playerId", idArg), ("pos", wPosArg)])
 RequestQuit   = defineMessageType("request_quit", [])
 SetPos        = defineMessageType("set_pos",
-                                  [("playerId", idArg), ("pos", posArg)])
+                                  [("playerId", idArg), ("pos", wPosArg)])
 YourIdIs      = defineMessageType("your_id_is", [("playerId", idArg)])
