3731bc64f1cfbabfa734aa82c5b6f93d32e57f5c
Greg Kronmiller <kronmillerg@gmail.com>
2017-08-19 15:07:48 -0700

    Add an extra message if the lint passes.

--- bin/lint.sh
+++ bin/lint.sh
@@ -5,3 +5,6 @@ set -e
 source "$(dirname "$(readlink -e "$0")")/head.sh"
 "${VIRTUALENV}/bin/pylint" --rcfile=src/.pylintrc src "$@"
 
+# We set -e, so if we make it this far then the lint must have passed.
+echo "10/10, would lint again."
+
