3df58c94902015762622a54e0e2d195638b6f2f3
Greg Kronmiller <kronmillerg@gmail.com>
2017-08-26 13:49:03 -0700

    Skip extra "lint passed" message in quiet mode.

--- bin/lint.sh
+++ bin/lint.sh
@@ -6,5 +6,9 @@ source "$(dirname "$(readlink -e "$0")")/head.sh"
 "${VIRTUALENV}/bin/pylint" --rcfile=src/.pylintrc src "$@"
 
 # We set -e, so if we make it this far then the lint must have passed.
-echo "10/10, would lint again."
+# But if there were command line arguments, then we may be running in quiet
+# mode, so to be safe, skip this message.
+if (( $# == 0 )); then
+    echo "10/10, would lint again."
+fi
 
